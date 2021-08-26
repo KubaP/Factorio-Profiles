fbf52d23c40750bf1c45191947f23ea20573e5a9
KubaP <git@kubap.io>
2021-08-26 22:07:16 +0100

    feat: Get general settings
    - Add ability for `Get-FactorioProfile` to display the general settings.

--- FactorioProfiles/src/GetFactorioProfileCommand.cs
+++ FactorioProfiles/src/GetFactorioProfileCommand.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 
 namespace FactorioProfiles
 {
-	[Cmdlet(VerbsCommon.Get, "FactorioProfile")]
+	[Cmdlet(VerbsCommon.Get, "FactorioProfile", DefaultParameterSetName = "Profile")]
 	[OutputType(typeof(Profile))]
 	public class GetFactorioProfile : PSCmdlet
 	{
-		[Parameter(Position = 0)]
+		[Parameter(Position = 0, ParameterSetName = "Profile")]
 		[ArgumentCompleter(typeof(NameCompleter))]
-		[Alias("Name")]
-		[AllowNull]
-		public String[] Names { get; set; }
+		[Alias("Profile", "Name", "Names")]
+		public String[] Profiles { get; set; }
+
+		[Parameter(Position = 0, ParameterSetName = "General")]
+		public SwitchParameter GeneralSettings { get; set; }
 
 		// BEGIN Block - Runs at the beginning of this cmdlet.
 		protected override void BeginProcessing() { }
@@ -20,33 +22,76 @@ namespace FactorioProfiles
 		// PROCESS Block - Runs once for each pipeline input.
 		protected override void ProcessRecord()
 		{
-			var profiles = new List<Profile>();
+			switch (ParameterSetName)
+			{
+				case "Profile":
+					var profiles = new List<Profile>();
 
-			// If no name is provided, display all profiles.
-			if (Names == null)
-			{
-				profiles = Data.GetProfiles();
-			}
-			else
-			{
-				// Iterate through all the given in names, and try to retrieve the corresponding profile.
-				foreach (String name in Names)
-				{
-					var profile = Data.GetProfile(name);
-					// If the profile doesn't exist, warn the user.
-					if (profile == null)
+					// If no name is provided, display all profiles.
+					if (Profiles == null)
 					{
-						WriteWarning($"There is no profile named '{name}'.");
-						continue;
+						profiles = Data.GetProfiles();
+					}
+					else
+					{
+						// Iterate through all the given in names, and try to retrieve the corresponding profile.
+						foreach (String name in Profiles)
+						{
+							var profile = Data.GetProfile(name);
+							// If the profile doesn't exist, warn the user.
+							if (profile == null)
+							{
+								WriteWarning($"There is no profile named '{name}'.");
+								continue;
+							}
+
+							profiles.Add(profile);
+						}
 					}
 
-					profiles.Add(profile);
-				}
-			}
+					// Sort the list by alphabetical order of the profile names.
+					profiles.Sort((x, y) => String.Compare(x.Name, y.Name));
+					WriteObject(profiles);
 
-			// Sort the list by alphabetical order of the profile names.
-			profiles.Sort((x, y) => String.Compare(x.Name, y.Name));
-			WriteObject(profiles);
+					break;
+				case "General":
+					// Display the general settings information.
+					WriteObject($"Path for new profiles: {Data.GetNewProfileSavePath()}");
+					var settings = Data.GetNewProfileSharingSettings();
+					var str = "";
+					if (settings.ShareConfig)
+					{
+						str += "Config, ";
+					}
+					if (settings.ShareMods)
+					{
+						str += "Mods, ";
+					}
+					if (settings.ShareSaves)
+					{
+						str += "Saves, ";
+					}
+					if (settings.ShareScenarios)
+					{
+						str += "Scenarios, ";
+					}
+					if (settings.ShareBlueprints)
+					{
+						str += "Blueprints, ";
+					}
+					str = str.Remove(str.Length - 2, 2);
+					WriteObject($"Sharing settings for new profiles:\n\t{str}");
+
+					break;
+				default:
+					ThrowTerminatingError(
+							new ErrorRecord(
+								new PSArgumentException("The current parameter set has fallen into an invalid set! This should never happen!"),
+								"1",
+								ErrorCategory.InvalidArgument,
+								null));
+					break;
+			}
 		}
 
 		// END Block - Runs at the end of this cmdlet.
